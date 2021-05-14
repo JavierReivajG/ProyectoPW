318e1b8190b3570df708da8e30637fc908b52064
JavierReivajG <jafg84alejandro@gmail.com>
2021-05-14 10:24:29 -0500

    si jalo el node, despues de tantos corajes xd

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require ('express');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const PassportLocal = require('passport-local').Strategy;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(cookieParser('mi ultra hiper secreto'));
+
+app.use(session({
+    secret: 'mi ultra hiper secreto',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new PassportLocal(function(username,password,done){
+    if(username === "123" && password ==="123")
+    return done (null, {id:1,name:"javier" });
+
+    done(null,false);
+}));
+
+
+passport.serializeUser(function(user,done){
+    done(null,user,id);
+});
+
+passport.deserializeUser(function(id,done){
+    done(null,{id: 1,name:"javier"});
+});
+
+
+app.set('view engine', 'ejs');
+
+app.get("/",(req,res)=> {
+    //si ya iniciamos mostrar bienvenida
+
+    //si no, mostrar login
+    app.send ("hola");
+});
+
+app.get("/login",(req,res)=>{
+    // mostrar el login
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate({
+    successRedirect: '/',
+    failureRedirect: "/login"
+}));
+
+
+
+app.listen(8080,()=> console.log("Server started"));
