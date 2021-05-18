644f4bae7b98524018bb2237c183564155363097
JavierReivajG <jafg84alejandro@gmail.com>
2021-05-18 13:38:06 -0500

    reparando el html y el css, ahora si ya a ponerle JS

--- src/js/Sing_in.js
+++ src/js/Sing_in.js
@@ -1,40 +1,13 @@
 
-var nombre = "";
-var correo = "123";
-var contra = "123";
 
-function iniciarsesion() 
-{
-    var a = document.getElementById('username').value;
-    var b = document.getElementById('password').value;
-
-
-    if (correo == a)
-    {
-        if (contra == b)
-        {
-            alert("Bienvenido");
-        }
-        else
-        {
-            alert("Contraseña incorrecta");
-        }
-    } 
-    else
-    {
-        alert("Este correo no existe");
-    }
-}
+var myInput = document.getElementById("username");
 
 
 
-function registrarse()
-{
-nombre = document.getElementById('rnomnbre').value;
-correo = document.getElementById('remail').value;
-contra = document.getElementById('rcontra').value;
-console.log(contra);
-console.log(nombre);
-console.log(correo);
 
-}
+
+myInput.onblour = function iniciarsesion() {
+    console.log("ya we")
+  }
+
+
