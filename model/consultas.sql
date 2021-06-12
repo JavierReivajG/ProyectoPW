f6b77a33776cd2f5f3a3c0841cab6917d4f0a466
arath-mrqz <arathmkz10@gmail.com>
2021-06-12 10:28:35 -0600

    nuevas consultas

--- model/consultas.sql
+++ model/consultas.sql
@@ -1,13 +1,98 @@
--- EJEMPLO CONSULTA --
-SELECT r.casilla_id,puesto,municipio,p.nombre,no_votos
-FROM registros r
-JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
-JOIN partidos p ON r.partido_id = p.partido_id
-JOIN casillas c ON c.casilla_id = r.casilla_id;
+USE prep;
 
-SELECT r.casilla_id,alianza, puesto, SUM(no_votos) Total_Votos
+-- CONSULTA VOTOS DE LAS ALIANZAS -- 
+SELECT r.casilla_id,puesto,alianza AS "Alianza", SUM(no_votos) Total_Votos
 FROM registros r
 JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
 JOIN partidos p ON r.partido_id = p.partido_id
 JOIN casillas c ON c.casilla_id = r.casilla_id
-GROUP BY r.casilla_id,alianza,puesto;
+WHERE alianza IS NOT NULL
+GROUP BY r.casilla_id,alianza,puesto
+ORDER BY puesto,Total_Votos DESC;
+
+-- CONSULTA VOTOS DE LOS PARTIDOS -- 
+SELECT r.casilla_id,puesto,nombre AS "Partido", SUM(no_votos) as Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE p.nombre != p.alianza
+GROUP BY r.casilla_id,nombre,puesto
+ORDER BY puesto,Total_Votos DESC;
+
+-- CONSULTA VOTOS DE LOS PARTIDOS Y ALIANZAS JUNTOS --
+SELECT r.casilla_id,puesto,alianza AS "Alianza o Partido", SUM(no_votos) Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE alianza IS NOT NULL
+GROUP BY r.casilla_id,alianza,puesto
+UNION
+SELECT r.casilla_id,puesto,nombre AS "Alianza o Partido", SUM(no_votos) as Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE p.alianza IS NULL
+GROUP BY r.casilla_id,nombre,puesto
+ORDER BY puesto,Total_Votos DESC;
+
+
+-- CONSULTA VOTOS DE LAS ALIANZAS Y PARTIDOS A DIPUTACIÓN FEDEREAL
+
+SELECT r.casilla_id,puesto,alianza AS "Alianza o Partido", SUM(no_votos) Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE alianza IS NOT NULL and cn.puesto = 'Diputado Federal'
+GROUP BY r.casilla_id,alianza,puesto
+UNION
+SELECT r.casilla_id,puesto,nombre AS "Alianza o Partido", SUM(no_votos) as Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE p.alianza IS NULL and cn.puesto = 'Diputado Federal'
+GROUP BY r.casilla_id,nombre,puesto
+ORDER BY puesto,Total_Votos DESC;
+
+-- CONSULTA VOTOS DE LAS ALIANZAS Y PARTIDOS A DIPUTACIÓN LOCAL
+
+SELECT r.casilla_id,puesto,alianza AS "Alianza o Partido", SUM(no_votos) Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE alianza IS NOT NULL and cn.puesto = 'Diputado Local'
+GROUP BY r.casilla_id,alianza,puesto
+UNION
+SELECT r.casilla_id,puesto,nombre AS "Alianza o Partido", SUM(no_votos) as Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE p.alianza IS NULL and cn.puesto = 'Diputado Local'
+GROUP BY r.casilla_id,nombre,puesto
+ORDER BY puesto,Total_Votos DESC;
+
+
+-- CONSULTA VOTOS DE LAS ALIANZAS Y PARTIDOS A PRESIDENCIA MUNICIPAL
+
+SELECT r.casilla_id,puesto,alianza AS "Alianza o Partido", SUM(no_votos) Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE alianza IS NOT NULL and cn.puesto = 'Presidente Municipal'
+GROUP BY r.casilla_id,alianza,puesto
+UNION
+SELECT r.casilla_id,puesto,nombre AS "Alianza o Partido", SUM(no_votos) as Total_Votos
+FROM registros r
+JOIN candidaturas cn ON cn.candidatura_id = r.candidatura_id
+JOIN partidos p ON r.partido_id = p.partido_id
+JOIN casillas c ON c.casilla_id = r.casilla_id
+WHERE p.alianza IS NULL and cn.puesto = 'Presidente Municipal'
+GROUP BY r.casilla_id,nombre,puesto
+ORDER BY puesto,Total_Votos DESC;
--- model/prep_inserts.sql
+++ model/prep_inserts.sql
@@ -24,7 +24,7 @@ INSERT INTO partidos VALUES(17,'Va Por México','Va Por México');
 INSERT INTO candidaturas VALUES(1,'Presidente Municipal','Tonala',11);
 INSERT INTO candidaturas VALUES(2,'Diputado Federal',null,null);
 INSERT INTO candidaturas VALUES(3,'Diputado Local','Tonala',11);
-
+INSERT INTO registros VALUES(9,1,5,10,1213);
 -- INSERT EJEMPLO CASILLA --
 /*
 INSERT INTO casillas VALUES(1213,2683,'normal');
@@ -35,7 +35,6 @@ INSERT INTO registros VALUES(2,1,2,5,1213);
 INSERT INTO registros VALUES(3,1,3,5,1213);
 INSERT INTO registros VALUES(4,1,16,5,1213);
 INSERT INTO registros VALUES(5,1,4,10,1213);
-
 INSERT INTO registros VALUES(6,2,3,100,1213);
-
+INSERT INTO registros VALUES(7,1,8,20,1213);
 */
