a14629739e94636d7711fa498221e636adedc8ce
Angel de León <isoukha.re@gmail.com>
2021-06-21 21:52:48 -0500

    Conexion a BD para Hosting

--- MVC/src/java/Config/Conexion.java
+++ MVC/src/java/Config/Conexion.java
@@ -6,11 +6,11 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 public class Conexion {
     public DriverManagerDataSource Conectar (){
         String User = "root";//Controlador.Email;
-        String Pass = "3366";//Controlador.Pass;
+        String Pass = "JSs4aXG5RP";//Controlador.Pass;
         
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3307/prep");
+        dataSource.setUrl("jdbc:mysql://node72028-env-5389750.jelastic.saveincloud.net:3306/prep");
         dataSource.setUsername(User); //request.getSession().setAttribute("usuario", "correo@gmail.com")
         dataSource.setPassword(Pass); //request.getSession().removeAttribute("usuario");
         return dataSource;
